5ead516e50a15867d0d91a738065b64ed2dda6f6
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-04-23 17:54:52 +0600

    update

--- Practice1/HelloWorld.java
+++ Practice1/HelloWorld.java
@@ -3,8 +3,14 @@ package Practice1;
 public class HelloWorld {
     public static void main(String[] args) {
         int age =30;
-        String sentence = "this is good";
-        System.out.println(age);
-        System.out.println(sentence);
+        if(age>10){
+            System.out.println("This is better");
+        }
+        else if(age == 10){
+            System.out.println("This is equal");
+        }
+        else{
+            System.out.println("This is should be 30");
+        }
     }
 }
