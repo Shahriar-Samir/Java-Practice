abb1225bf9d953ca4b1b2d365b75b667a7f9e7f5
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-04-23 17:34:27 +0600

    added: practice1

---
+++ Practice1/HelloWorld.java
@@ -0,0 +1,10 @@
+package Practice1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        int age =30;
+        String sentence = "this is good";
+        System.out.println(age);
+        System.out.println(sentence);
+    }
+}
