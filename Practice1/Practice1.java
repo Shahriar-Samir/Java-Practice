047f0934de274aa7a9a8a77862eab075e0202729
Shahriar-Samir <shahriar.samir80@gmail.com>
2025-04-24 09:15:44 +0600

    update

---
+++ Practice1/Practice1.java
@@ -0,0 +1,12 @@
+package Practice1;
+
+public class Practice1 {
+    public static void main(String[] args) {
+        int num1 = 34;
+        Double num2 = 3.5;
+        char oneChar = 'o';
+        String line1 = "This is Good";
+        boolean isFixed = true;
+        System.out.println("Hello from Java!"+" "+num1+ " "+ num2+ oneChar + " " + line1 + "" + isFixed);
+    }
+}
